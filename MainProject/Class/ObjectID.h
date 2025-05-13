1c06c91af787b656393693b738011d315a4c7db2
sasakisasa2 <nanami.ahiru@gmail.com>
2025-05-13 17:19:53 +0900

    クラスと名称が重なるので、ID名を変更

--- MainProject/Class/ObjectID.h
+++ MainProject/Class/ObjectID.h
@@ -6,15 +6,15 @@ namespace ObjectID
 	};
 	enum Character
 	{
-		Player = BackGroundForest+1,
-		Enemy
+		PlayerID = BackGroundForest+1,
+		EnemyID
 	};
 	enum Effects
 	{
-		PlayerEffect = Enemy + 1
+		PlayerEffectID = Enemy + 1
 	};
 
-	static constexpr int MAX_ID_NUMBER = PlayerEffect + 1;
+	static constexpr int MAX_ID_NUMBER = PlayerEffectID + 1;
 
 	static const wchar_t* SpriteName[MAX_ID_NUMBER] = 
 	{
