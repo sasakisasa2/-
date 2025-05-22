2337e5427fc551135139b755bbc20b7e0cf5a9d5
sasakisasa2 <nanami.ahiru@gmail.com>
2025-05-22 11:03:58 +0900

    プレイヤーが移動できるように

--- MainProject/Class/Player.cpp
+++ MainProject/Class/Player.cpp
@@ -9,5 +9,31 @@ void Player::SetDate()
 void Player::UpDate()
 {
 	Vector2 pos = GetPosition(PlayerID);
+
+	Vector2 move;
+
+	if (InputSystem.Keyboard.isPressed.W)
+	{
+		move.y -= speed * DXTK->Time.deltaTime;
+	}
+	if (InputSystem.Keyboard.isPressed.A)
+	{
+		move.x -= speed * DXTK->Time.deltaTime;
+	}
+	if (InputSystem.Keyboard.isPressed.S)
+	{
+		move.y += speed * DXTK->Time.deltaTime;
+	}
+	if (InputSystem.Keyboard.isPressed.D)
+	{
+		move.x += speed * DXTK->Time.deltaTime;
+	}
+
+	move.Normalize();
+
+	pos += move;
+
+	SetPosition(pos, PlayerID);
+
 	anime.UpDate();
 }
