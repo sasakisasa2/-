80fabbd7364163e545dd6ced528fca94a13c9a06
sasakisasa2 <nanami.ahiru@gmail.com>
2025-05-22 11:20:41 +0900

    画面外へ出ないように

--- MainProject/Class/Player.cpp
+++ MainProject/Class/Player.cpp
@@ -13,25 +13,36 @@ void Player::UpDate()
 	Vector2 move;
 
 	if (InputSystem.Keyboard.isPressed.W)
-	{
 		move.y -= speed * DXTK->Time.deltaTime;
-	}
 	if (InputSystem.Keyboard.isPressed.A)
-	{
 		move.x -= speed * DXTK->Time.deltaTime;
-	}
 	if (InputSystem.Keyboard.isPressed.S)
-	{
 		move.y += speed * DXTK->Time.deltaTime;
-	}
 	if (InputSystem.Keyboard.isPressed.D)
-	{
 		move.x += speed * DXTK->Time.deltaTime;
-	}
 
 	move.Normalize();
 
 	pos += move;
+
+	Vector4	border;
+
+	border = Vector4
+	(
+		((float)GetSprite(PlayerID).size.x / GetImagesNumber(PlayerID).x) / 2.0f,
+		((float)GetSprite(PlayerID).size.y / GetImagesNumber(PlayerID).y) / 2.0f,
+		DXTK->SwapChain.Width  - ((float)GetSprite(PlayerID).size.x / GetImagesNumber(PlayerID).x) / 2.0f,
+		DXTK->SwapChain.Height - ((float)GetSprite(PlayerID).size.y / GetImagesNumber(PlayerID).y) / 2.0f
+	);
+
+	if (pos.x > border.z)//右端
+		pos.x = border.z;
+	if (pos.x < border.x)//左端
+		pos.x = border.x;
+	if (pos.y > border.w)//下端
+		pos.y = border.w;
+	if (pos.y < border.y)//上端
+		pos.y = border.y;
 
 	SetPosition(pos, PlayerID);
 
