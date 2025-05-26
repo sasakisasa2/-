e99109e2efdc910d8938ad8249271dbffd31520d
sasakisasa2 <nanami.ahiru@gmail.com>
2025-05-26 17:00:27 +0900

    当たり判定をする関数を作成

--- MainProject/Class/CollisionDetection.cpp
+++ MainProject/Class/CollisionDetection.cpp
@@ -1,11 +1,17 @@
 #include "CollisionDetection.h"
 
-void CollisionDetection::SetDate()
-{ 
-}
-
-void CollisionDetection::UpDate()
+bool CollisionDetection::Detection(int ID, int ID2)
 {
+	float ratio = sqrt
+	(
+		(GetPosition(ID).x - GetPosition(ID2).x) *
+		(GetPosition(ID).x - GetPosition(ID2).x) +
+		(GetPosition(ID).y - GetPosition(ID2).y) *
+		(GetPosition(ID).y - GetPosition(ID2).y)
+	);
+
+	if(ratio <= 10.0f)
+		return true;
 }
 
 void CollisionDetection::RegisterCollision(SpriteRender* object)
--- MainProject/Class/CollisionDetection.h
+++ MainProject/Class/CollisionDetection.h
@@ -6,9 +6,11 @@ class CollisionDetection:public SpriteRender
 private:
 public:
 	CollisionDetection(){ }
-	~CollisionDetection() { Initialize(EnemyID); }
-	void SetDate()override;
-	void UpDate()override;
+	~CollisionDetection() { }
+	void SetDate()override {}
+	void UpDate()override {}
+
+	bool Detection(int ID, int ID2);
 
 	void RegisterCollision(SpriteRender* object);
 };
