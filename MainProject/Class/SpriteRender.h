586db1df484ea659aeb265b567ccccba4f19ff8a
sasakisasa2 <nanami.ahiru@gmail.com>
2025-05-29 10:32:58 +0900

    Getter,Setter関数をpublicに

--- MainProject/Class/SpriteRender.h
+++ MainProject/Class/SpriteRender.h
@@ -30,37 +30,37 @@ protected:
 	//子クラス初期化用関数
 	static void Initialize(int ID);
 
+public:
+
 #pragma region Getter関数
 
-	static DirectXTK::Sprite const GetSprite  (int ID) { return sprite      [ID]; }
-	static Vector2       const GetPosition    (int ID) { return position    [ID]; }
-	static Vector2       const GetImagesCount (int ID) { return imagesCount [ID]; }
+	static DirectXTK::Sprite const GetSprite  (int ID) { return sprite[ID];       }
+	static Vector2       const GetPosition    (int ID) { return position[ID];     }
+	static Vector2       const GetImagesCount (int ID) { return imagesCount[ID];  }
 	static Vector2       const GetImagesNumber(int ID) { return imagesNumber[ID]; }
-	static Color         const GetColor       (int ID) { return color       [ID]; }
-	static float         const GetAngle       (int ID) { return angle       [ID]; }
-	static SpriteEffects const GetSpEffect    (int ID) { return spEffect    [ID]; }
+	static Color         const GetColor       (int ID) { return color[ID];        }
+	static float         const GetAngle       (int ID) { return angle[ID];        }
+	static SpriteEffects const GetSpEffect    (int ID) { return spEffect[ID];     }
 
 #pragma endregion
 
 #pragma region Setter関数
 
-	static void SetSpriteSize(Vector2 setSize, int ID) 
-	{ 
-		sprite[ID].size.x = setSize.x; 
+	static void SetSpriteSize(Vector2 setSize, int ID)
+	{
+		sprite[ID].size.x = setSize.x;
 		sprite[ID].size.y = setSize.y;
 	}
-	static void SetPosition    (Vector2 setPos,   int ID) { position[ID]     = setPos;    }
-	static void SetImagesCount (Vector2 setCount, int ID) { imagesCount[ID]  = setCount;  }
-	static void SetImagesNumber(Vector2 setNumber,int ID) { imagesNumber[ID] = setNumber; }
-	static void SetColor       (Color   setColor, int ID) { color[ID]        = setColor;  }
-	static void SetAngle       (float   setAngle, int ID) { angle[ID]        = setAngle;  }
-	static void SetSpEffect    (SpriteEffects setEffect, int ID) { spEffect[ID] = setEffect; }
-	static void SetIsRender    (bool    setIsRen, int ID) { isRender[ID]     = setIsRen;  }
+	static void SetPosition    (Vector2 setPos, int ID)          { position[ID] = setPos;        }
+	static void SetImagesCount (Vector2 setCount, int ID)        { imagesCount[ID] = setCount;   }
+	static void SetImagesNumber(Vector2 setNumber, int ID)       { imagesNumber[ID] = setNumber; }
+	static void SetColor       (Color   setColor, int ID)        { color[ID] = setColor;         }
+	static void SetAngle       (float   setAngle, int ID)        { angle[ID] = setAngle;         }
+	static void SetSpEffect    (SpriteEffects setEffect, int ID) { spEffect[ID] = setEffect;     }
+	static void SetIsRender    (bool    setIsRen, int ID)        { isRender[ID] = setIsRen;      }
 
 #pragma endregion
 
-
-public:
     SpriteRender() { Initialize(); }
 	virtual ~SpriteRender(){ }
 
