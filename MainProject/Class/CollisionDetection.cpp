156d8d38d1f3b08cfb9165848d396a6fae6520f1
sasakisasa2 <nanami.ahiru@gmail.com>
2025-06-06 16:20:20 +0900

    ereseで当たり判定時mapから削除するように クラス破壊の関数の削除

--- MainProject/Class/CollisionDetection.cpp
+++ MainProject/Class/CollisionDetection.cpp
@@ -14,29 +14,25 @@ bool CollisionDetection::Detection(Vector2 ob1, Vector2 ob2)
 	return false;
 }
 
-void CollisionDetection::Detection(int ID, SpriteRender* ob)
+void CollisionDetection::Detection(int ID, SpriteRender*& ob)
 {
 	cmap[ID].emplace_back(ob);
 }
 
-void CollisionDetection::RegisterCollision(SpriteRender*& object)
-{
-	delete object;
-	object = nullptr;
-}
-
 void CollisionDetection::RegisterCollision(int ID1, int ID2)
 {
-	std::vector<SpriteRender*>& cd1 = cmap[ID1];
-	std::vector<SpriteRender*>& cd2 = cmap[ID2];
+	auto&& cd1 = cmap[ID1];
+	auto&& cd2 = cmap[ID2];
 	for (int i = 0; i < cd1.size(); i++)
 	{
 		for (int j = 0; j < cd2.size(); j++) 
 		{
-			if (Detection(cd1[i]->GetPosition(ID1), cd2[j]->GetPosition(ID2)))
+			if (Detection(cd1[i]->GetPosition(ID1+i), cd2[j]->GetPosition(ID2+j)))
 			{
 				cd1[i]->Collision();
 				cd2[j]->Collision();
+				cmap.erase(ID1);
+				cmap.erase(ID2);
 			}
 		}
 	}
--- MainProject/Class/CollisionDetection.h
+++ MainProject/Class/CollisionDetection.h
@@ -10,9 +10,8 @@ public:
 	~CollisionDetection() { }
 
 	bool Detection(Vector2 ob1, Vector2 ob2);
-	void Detection(int ID, SpriteRender*);
+	void Detection(int ID, SpriteRender*&);
 
-	void RegisterCollision(SpriteRender*& object);
 	void RegisterCollision(int ID1, int ID2);
 
 	std::map<int,std::vector<SpriteRender*>> cmap;
