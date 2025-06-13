eb1381d007fdb326dcb2208732d47d52741280e7
sasakisasa2 <nanami.ahiru@gmail.com>
2025-06-13 15:22:38 +0900

    object数を入れる変数の定義

--- MainProject/Class/ObjectID.h
+++ MainProject/Class/ObjectID.h
@@ -6,13 +6,20 @@ namespace ObjectID
 	};
 	enum Character
 	{
-		PlayerID = BGID +1,
+		PlayerID = BGID+1,
 		EnemyID
 	};
 	enum Effects
 	{ };
 
 	static constexpr int MAX_ID_NUMBER = EnemyID + 1;
+
+	static constexpr int MAX_OBJECT_NUMBER[MAX_ID_NUMBER] =
+	{
+		1,  //BG
+		1,  //Player
+		100 //Enemy
+	};
 
 	static const wchar_t* SpriteName[MAX_ID_NUMBER] = 
 	{
