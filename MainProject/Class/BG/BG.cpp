2a3c86157f208e86bcf7d0b18662ba903b7cfe1f
sasakisasa2 <nanami.ahiru@gmail.com>
2025-06-16 15:43:41 +0900

    BGクラスをmap用に変更

--- MainProject/Class/BG/BG.cpp
+++ MainProject/Class/BG/BG.cpp
@@ -1,29 +1,29 @@
 #include "BG.h"
 
-void BG::SetDate()
+void BG::SetDate(int count)
 {
 	SetPosition
 	(
 		//Œ´“_‚ª^‚ñ’†‚Ìˆ×ˆÚ“®
 		Vector2
 		(
-			(float)GetSprite(BackGroundForest).size.x / 2.0f,
+			(float)GetSprite(BGID).size.x / 2.0f,
 			(float)DXTK->SwapChain.Height / 2.0f
 		),
-		BackGroundForest
+		BGID
 	);
 }
 
-void BG::UpDate()
+void BG::UpDate(int count)
 {
-	Vector2 pos = GetPosition(BackGroundForest);
+	Vector2 pos = GetPosition(BGID,count);
 
 	pos.x -= speed * DXTK->Time.deltaTime;
 	
 	if (pos.x <= -(float)DXTK->SwapChain.Width / 2.0f)
 	{
-		pos.x = (float)GetSprite(BackGroundForest).size.x / 2.0f;
+		pos.x = (float)GetSprite(BGID).size.x / 2.0f;
 	}
 
-	SetPosition(pos, BackGroundForest);
+	SetPosition(pos, BGID,count);
 }
--- MainProject/Class/BG/BG.h
+++ MainProject/Class/BG/BG.h
@@ -13,7 +13,7 @@ private:
 
 public:
 	BG():speed(500.0f){ }
-	void SetDate()override;
-	void UpDate()override;
-	void Collision() {};
+	void SetDate  (int count)override;
+	void UpDate   (int count)override;
+	void Collision(int count)override {};
 };
