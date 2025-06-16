37d22911237ef768a66f4537a9cfa2772374bf98
sasakisasa2 <nanami.ahiru@gmail.com>
2025-06-16 15:50:36 +0900

    playerAnimationクラスをmap用に変更

--- MainProject/Class/Player/Animation/PlayerAnimation.cpp
+++ MainProject/Class/Player/Animation/PlayerAnimation.cpp
@@ -1,28 +1,28 @@
 #include "PlayerAnimation.h"
 
-void PlayerAnimation::SetDate()
+void PlayerAnimation::SetDate(int count)
 {
-	SetImagesNumber(Vector2(9, 1), PlayerID);
+	SetImagesNumber(Vector2(9, 1), PlayerID,count);
 }
 
-void PlayerAnimation::UpDate()
+void PlayerAnimation::UpDate(int count)
 {
 	time += DXTK->Time.deltaTime;
 
 	if (time >= END_TIME)
 	{
-		Vector2 count = GetImagesCount(PlayerID);
+		Vector2 imageCount = GetImagesCount(PlayerID, count);
 
 		time = 0.0f;
 
-		if (isSwitch) { count.x--; }
-		else          { count.x++; }
+		if (isSwitch) { imageCount.x--; }
+		else          { imageCount.x++; }
 		
-		if (count.x == GetImagesNumber(PlayerID).x-1)
+		if (imageCount.x == GetImagesNumber(PlayerID,count).x-1)
 			isSwitch = true;
-		if (count.x == 0.0f)
+		if (imageCount.x == 0.0f)
 			isSwitch = false;
 
-		SetImagesCount(count, PlayerID);
+		SetImagesCount(imageCount, PlayerID,count);
 	}
 }
--- MainProject/Class/Player/Animation/PlayerAnimation.h
+++ MainProject/Class/Player/Animation/PlayerAnimation.h
@@ -9,9 +9,8 @@ private:
 	const float END_TIME = 1;
 public:
 	PlayerAnimation():time(0),isSwitch(false){}
-	~PlayerAnimation() = default;
-	void SetDate()override;
-	void UpDate()override;
-	void Collision() {};
-
+	~PlayerAnimation()override = default;
+	void SetDate  (int count)override;
+	void UpDate   (int count)override;
+	void Collision(int count)override {};
 };
