9cd162b358d945ce65829207c9d8658c624ebc2a
sasakisasa2 <nanami.ahiru@gmail.com>
2025-06-16 16:36:40 +0900

    mapの削除を関数でやるように

--- MainProject/Class/Collision/CollisionDetection.cpp
+++ MainProject/Class/Collision/CollisionDetection.cpp
@@ -21,19 +21,25 @@ void CollisionDetection::Detection(int ID, GameObject*& ob)
 
 void CollisionDetection::RegisterCollision(int ID1, int ID2)
 {
+	int eraseCount[2]{ 0,0 };
 	auto&& cd1 = cmap[ID1];
 	auto&& cd2 = cmap[ID2];
 	for (int i = 0; i < cd1.size(); i++)
 	{
 		for (int j = 0; j < cd2.size(); j++) 
 		{
-			if (Detection(cd1[i]->GetPosition(ID1+i), cd2[j]->GetPosition(ID2+j)))
+			if (Detection(cd1[i]->GetPosition(ID1,i), cd2[j]->GetPosition(ID2,j)))
 			{
+				eraseCount[0]++;
+				eraseCount[1]++;
 				cd1[i]->Collision(ID1);
 				cd2[j]->Collision(ID2);
-				cmap.erase(ID1);
-				cmap.erase(ID2);
 			}
 		}
 	}
+}
+
+void CollisionDetection::MapErase(int ID)
+{
+	cmap.erase(ID);
 }
--- MainProject/Class/Collision/CollisionDetection.h
+++ MainProject/Class/Collision/CollisionDetection.h
@@ -13,6 +13,7 @@ public:
 	void Detection(int ID, GameObject*&);
 
 	void RegisterCollision(int ID1, int ID2);
+	void MapErase(int ID);
 
 	std::map<int,std::vector<GameObject*>> cmap;
 };
