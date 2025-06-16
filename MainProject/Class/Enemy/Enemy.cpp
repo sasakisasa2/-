9625b3e6ab8b302761c348985c2be4b6251d660f
sasakisasa2 <nanami.ahiru@gmail.com>
2025-06-16 17:37:29 +0900

    位置がランダムになるように

--- MainProject/Class/Enemy/Enemy.cpp
+++ MainProject/Class/Enemy/Enemy.cpp
@@ -2,15 +2,19 @@
 
 void Enemy::SetDate(int count)
 { 
-	SetPosition(Vector2(600.0f, 600.0f), EnemyID,count);
-	SetSpriteSize(XMUINT2(100.0f, 100.0f), EnemyID, count);
+	std::random_device rand_dev; 
+	std::mt19937                          random = std::mt19937(rand_dev());
+	std::uniform_real_distribution<float> x_pos_range_ = std::uniform_real_distribution<float>(0.0f, 1200.0f);
+	std::uniform_real_distribution<float> y_pos_range_ = std::uniform_real_distribution<float>(0.0f, 600.0f);
+	SetPosition(Vector2(x_pos_range_(random), y_pos_range_(random)), EnemyID, count);
+	SetSpriteSize(XMUINT2(100.0f, 100.0f), EnemyID);
 }
 
 void Enemy::UpDate(int count)
 {
 	Vector2 pos = GetPosition(EnemyID,count);
 
-	Vector2 move = GetPosition(PlayerID, count) - pos;
+	Vector2 move = GetPosition(PlayerID, 0) - pos;
 
 	move.Normalize();
 
--- MainProject/Class/Enemy/Enemy.h
+++ MainProject/Class/Enemy/Enemy.h
@@ -1,5 +1,6 @@
 #pragma once
 #include"../GameObject/GameObject.h"
+#include<random>
 
 class Enemy:public GameObject
 {
