f8a8e4b3e4bfe3603b594af3b083c07c363dd0d1
sasakisasa2 <nanami.ahiru@gmail.com>
2025-06-23 15:38:43 +0900

    ランダムな位置にエネミーを配置

--- MainProject/Class/Enemy/Enemy.cpp
+++ MainProject/Class/Enemy/Enemy.cpp
@@ -6,8 +6,11 @@ void Enemy::SetDate()
 	std::mt19937                          random = std::mt19937(rand_dev());
 	std::uniform_real_distribution<float> x_pos_range_ = std::uniform_real_distribution<float>(0.0f, 1200.0f);
 	std::uniform_real_distribution<float> y_pos_range_ = std::uniform_real_distribution<float>(0.0f, 600.0f);
-	SetPosition(Vector2(x_pos_range_(random), y_pos_range_(random)), EnemyID, count);
 	SetSpriteSize(XMUINT2(100.0f, 100.0f), EnemyID);
+	for (int i = 0; i < MAX_OBJECT_NUMBER[EnemyID]; i++)
+	{
+		SetPosition(Vector2(x_pos_range_(random), y_pos_range_(random)), EnemyID, i);
+	}
 }
 
 void Enemy::UpDate()
